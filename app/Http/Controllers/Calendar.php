4c427e37685f72a485ee3c4ef1215f7b6f136fa1
Mateusz Brzozowski <neertoon@icloud.com>
2019-09-11 21:55:33 +0200

    Pierwszy commit

---
+++ app/Http/Controllers/Calendar.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class Calendar extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param int $id
+     * @return View
+     */
+    public function index()
+    {
+        return 'OK: ';
+    }
+}
