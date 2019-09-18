02989f3b4ecd2fd7e158e5fdf7d5f647eec6323e
Mateusz Brzozowski <neertoon@icloud.com>
2019-09-18 18:45:14 +0200

    Prosta strona logowania

--- app/Http/Controllers/Calendar.php
+++ app/Http/Controllers/Calendar.php
@@ -6,6 +6,13 @@ use App\Http\Controllers\Controller;
 
 class Calendar extends Controller
 {
+    const APPLICATION_ID = '973852827368-4981aoak1r7car10lskp4jau4bm61khv.apps.googleusercontent.com';
+    private $application_redirect_url;
+    public function __construct() {
+        $this->application_redirect_url = 'http://'.$_SERVER['HTTP_HOST'].'/server.php';
+//        $this->application_redirect_url = 'server.php';
+    }
+
     /**
      * Show the profile for the given user.
      *
@@ -14,6 +21,26 @@ class Calendar extends Controller
      */
     public function index()
     {
-        return 'OK: ';
+        $data = array_merge($_GET, $_POST);
+        file_put_contents('server_data.txt', var_export($data, true));
+
+        $linkToSignIn  = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode($this->application_redirect_url) . '&response_type=code&client_id=' . self::APPLICATION_ID . '&access_type=online';
+
+        $data = [
+            'sign_in' => $linkToSignIn,
+        ];
+
+        $this->showView($data);
+    }
+
+    protected function showView($data) {
+        $out = <<<OUT
+
+<h1>Welcom on Cart Calendar</h1>
+<h2>Please <a href="{$data['sign_in']}">SIGN IN</a></h2>
+
+OUT;
+
+        echo $out;
     }
 }
