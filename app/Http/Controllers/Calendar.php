c0678c0741a90d449291118e77a793385c813845
Mateusz Brzozowski <neertoon@icloud.com>
2019-09-18 19:32:45 +0200

    autoryzacja google

--- app/Http/Controllers/Calendar.php
+++ app/Http/Controllers/Calendar.php
@@ -24,6 +24,17 @@ class Calendar extends Controller
         $data = array_merge($_GET, $_POST);
         file_put_contents('server_data.txt', var_export($data, true));
 
+        if(!empty($_GET['code'])) {
+            $client = new \Google_Client();
+            $client->setApplicationName('cart-calendar-253012');
+            $client->setScopes(\Google_Service_Calendar::CALENDAR);
+//            $client->setAuthConfig('credentials.json');
+            $client->setAuthConfig($this->getConfig());
+            $client->setAccessType('online');
+            $cred = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+            file_put_contents('server_token.txt', var_export($cred, true));
+        }
+
         $linkToSignIn  = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode($this->application_redirect_url) . '&response_type=code&client_id=' . self::APPLICATION_ID . '&access_type=online';
 
         $data = [
@@ -36,11 +47,16 @@ class Calendar extends Controller
     protected function showView($data) {
         $out = <<<OUT
 
-<h1>Welcom on Cart Calendar</h1>
+<h1>Welcom on Cart Calendar V18-1932</h1>
 <h2>Please <a href="{$data['sign_in']}">SIGN IN</a></h2>
 
 OUT;
 
         echo $out;
+    }
+
+    protected function getConfig() {
+        $json = '{"web":{"client_id":"973852827368-4981aoak1r7car10lskp4jau4bm61khv.apps.googleusercontent.com","project_id":"cart-calendar-253012","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"WchllqHP2kBNn1Gvqb82DZ6f","redirect_uris":["http://cart-calendar.azurewebsites.net/server.php"]}}';
+        return json_decode($json, true);
     }
 }
