693b2efaec9547e8f6de535068fdc4ff75d54f76
Mateusz Brzozowski <neertoon@icloud.com>
2019-09-23 09:43:30 +0200

    Zmiana w przekierowaniu na działający

--- app/Http/Controllers/Calendar.php
+++ app/Http/Controllers/Calendar.php
@@ -11,7 +11,7 @@ class Calendar extends Controller
     const APPLICATION_ID = '973852827368-4981aoak1r7car10lskp4jau4bm61khv.apps.googleusercontent.com';
     private $application_redirect_url;
     public function __construct() {
-        $this->application_redirect_url = 'http://'.$_SERVER['HTTP_HOST'].'/cart';
+        $this->application_redirect_url = 'http://'.$_SERVER['HTTP_HOST'].'/index.php/cart';
 //        $this->application_redirect_url = 'server.php';
     }
 
