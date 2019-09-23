edde3e4581107d92545851d1c83b5d5082e71d67
Mateusz Brzozowski <neertoon@icloud.com>
2019-09-23 12:25:54 +0200

    Nie wymagaj stale logowania - po ponownym wejściu na stronie trzymaj code do generowania tokena

--- app/Http/Controllers/Calendar.php
+++ app/Http/Controllers/Calendar.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Services\CalendarClient;
 use App\Services\Wydruk\GeneratorDom;
 use App\User;
 use App\Http\Controllers\Controller;
@@ -12,14 +13,16 @@ class Calendar extends Controller
     private $application_redirect_url;
     public function __construct() {
         $this->application_redirect_url = 'http://'.$_SERVER['HTTP_HOST'].'/index.php/cart';
-        //OK OK
-//        $this->application_redirect_url = 'server.php';
     }
 
     public function login() {
-        $linkToSignIn  = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode($this->application_redirect_url) . '&response_type=code&client_id=' . self::APPLICATION_ID . '&access_type=online';
+        if (empty($_SESSION['code'])) {
+            $linkToSignIn  = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode($this->application_redirect_url) . '&response_type=code&client_id=' . self::APPLICATION_ID . '&access_type=online';
 
-        echo view('login', ['link' => $linkToSignIn]);
+            echo view('login', ['link' => $linkToSignIn]);
+        } else {
+            $this->index();
+        }
     }
 
     /**
@@ -34,34 +37,30 @@ class Calendar extends Controller
         file_put_contents('server_data.txt', var_export($data, true));
 
         if(!empty($_GET['code'])) {
-            $client = new \Google_Client();
-            $client->setApplicationName('cart-calendar-253012');
-            $client->setScopes(\Google_Service_Calendar::CALENDAR);
-//            $client->setAuthConfig('credentials.json');
-            $client->setAuthConfig($this->getConfig());
-            $client->setAccessType('online');
-            $cred = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-            file_put_contents('server_token.txt', var_export($cred, true));
-
-            $service = new \Google_Service_Calendar($client);
-            // Print the next 10 events on the user's calendar.
-            $calendarId = 'primary';
-            $optParams = array(
-                'maxResults' => 10,
-                'orderBy' => 'startTime',
-                'singleEvents' => true,
-                'timeMin' => date('c'),
-            );
-            //timeMax date('c') tu będzie zabawa, bo bęzdie trzeba obliczyć początek tygodnia, w którym jest pierwszy dzień miesiąca i koniec miesiąca
-            $results = $service->events->listEvents($calendarId, $optParams);
-            $events = $results->getItems();
-
-            $calendars = $service->calendarList->listCalendarList();
-            echo '<pre>';
-//            var_export($calendars);
-            var_export($events);
-            echo '</pre>';
+            $_SESSION['code'] = $_GET['code'];
         }
+
+        $googleClient = new CalendarClient();
+        $client = $googleClient->get();
+
+        $service = new \Google_Service_Calendar($client);
+        // Print the next 10 events on the user's calendar.
+        $calendarId = 'primary';
+        $optParams = array(
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c'),
+        );
+        //timeMax date('c') tu będzie zabawa, bo bęzdie trzeba obliczyć początek tygodnia, w którym jest pierwszy dzień miesiąca i koniec miesiąca
+        $results = $service->events->listEvents($calendarId, $optParams);
+        $events = $results->getItems();
+
+        $calendars = $service->calendarList->listCalendarList();
+        echo '<pre>';
+//            var_export($calendars);
+        var_export($events);
+        echo '</pre>';
 
         $linkToSignIn  = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode($this->application_redirect_url) . '&response_type=code&client_id=' . self::APPLICATION_ID . '&access_type=online';
 
@@ -95,10 +94,5 @@ class Calendar extends Controller
 OUT;
 
         echo $out;
-    }
-
-    protected function getConfig() {
-        $json = '{"web":{"client_id":"973852827368-4981aoak1r7car10lskp4jau4bm61khv.apps.googleusercontent.com","project_id":"cart-calendar-253012","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"WchllqHP2kBNn1Gvqb82DZ6f","redirect_uris":["http://cart-calendar.azurewebsites.net/index.php/cart"]}}';
-        return json_decode($json, true);
     }
 }
---
+++ app/Services/CalendarClient.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Services;
+
+
+class CalendarClient {
+    public function get() {
+        $code = $_SESSION['code'];
+
+        $client = new \Google_Client();
+        $client->setApplicationName('cart-calendar-253012');
+        $client->setScopes(\Google_Service_Calendar::CALENDAR);
+        $client->setAuthConfig($this->getConfig());
+        $client->setAccessType('online');
+        $cred = $client->fetchAccessTokenWithAuthCode($code);
+        file_put_contents('server_token.txt', var_export($cred, true));
+        return $client;
+    }
+
+    protected function getConfig() {
+        $json = '{"web":{"client_id":"973852827368-4981aoak1r7car10lskp4jau4bm61khv.apps.googleusercontent.com","project_id":"cart-calendar-253012","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"WchllqHP2kBNn1Gvqb82DZ6f","redirect_uris":["http://cart-calendar.azurewebsites.net/index.php/cart"]}}';
+        return json_decode($json, true);
+    }
+}
